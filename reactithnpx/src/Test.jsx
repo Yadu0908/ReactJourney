d0d32c869cab5b3178ebe945aead03af78f05f7a
code_with_bot <programming.with.yadu@gmail.com>
2024-08-26 00:39:17 +0530

    create new react project

---
+++ reactithnpx/src/Test.jsx
@@ -0,0 +1,17 @@
+//Why we make this file with the .jsx extenstion name-: Because it's in the good practice if we have  some html elements into the js file then the name of the file should be ended with the jsx.
+
+//And the function name should always be started with the capital latter.
+
+function Test(){
+
+    return <>
+    
+    <h1>Hey this is my react project</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident nobis amet sit sunt cupiditate illum dolore delectus earum? Ducimus illum inventore perferendis repellat? Perferendis blanditiis magnam sunt eius enim asperiores?</p>
+    <button>Click me</button>
+       
+    </>
+
+}
+
+export default Test
