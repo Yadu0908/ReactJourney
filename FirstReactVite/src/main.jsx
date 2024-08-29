650145e74a139616122477cff11b38898f8e5161
code_with_bot <programming.with.yadu@gmail.com>
2024-08-29 21:53:11 +0530

    about JSX

--- FirstReactVite/src/main.jsx
+++ FirstReactVite/src/main.jsx
@@ -1,10 +1,58 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Test from './new.jsx'
+
+
+function MyApp(){
+    return (
+        <h1>Hello world working</h1>
+    )
+}
+
+//But when we try to render the object it will throw the error's on the console window. cuz the render function take an function as an argument not object.
+
+//Self defined object.
+
+// let domElement = {
+
+//     type: 'a',
+
+//     props: {
+
+//         href: 'https:www.google.com',
+//         target: '_blank'
+//     },
+
+//     children: "Hey click me to visit google"
+// }
+
+
+function Newfun(){
+    return(
+    <a href="http://www.google.com" target='_blank'>Visit google</a>
+    )
+}
+
+//Object defined as per the react.
+let Reactelement = React.createElement(
+
+    'a',
+    {
+        href: "https://www.google.com",
+        target: "_blank"
+    },
+
+    'click me to visit google'
+)
+
 
 createRoot(document.getElementById('root')).render(
  
-    <App />
+    // MyApp()      //we can execute our function like this
+
+    // domElement
   
+   <Test/>
 )
--- FirstReactVite/src/new.jsx
+++ FirstReactVite/src/new.jsx
@@ -1,9 +1,14 @@
 //The function name should be on the uppercase while creating the function on the jsx file.
 
 function Test(){
-
+    let variable = " by Yadu"
     
-    return<h1>Hey this is my custom message</h1>
+    return (
+        <>
+            {/* The syntex of brackting is known as evaluated expressions. */}
+            <h1>Hello world with Test {variable}</h1>   
+        </>
+    )
     
 }
 
